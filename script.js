558edb4ec1a7c37b9e9e4c576aa1219704e19dd9
Amirali Rezaei <amiralirezaie169@gmail.com>
2026-03-24 13:00:40 +0100

    edited some things ,after cheked with Samuel

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-const age = Number(prompt('Inserisci la tua età:'));
+let age = Number(prompt('Inserisci la tua età:'))
 const priceOfKm = 0.21;
-const distance =Number(prompt('Inserisci la distanza dalla tua destinazione(km):'));
-let price = distance * priceOfKm;
+let distance = Number(prompt('Inserisci la distanza dalla tua destinazione(km):'));
 
-if (age<=18) {
+if (isNaN(age) || isNaN(distance) || age <= 0 || distance <= 0) {
+    console.log("❌ Errore! Devi inserire numeri validi maggiori di 0.❌");
+}
+else { console.log("✅ Corretto"); 
+let price = distance * priceOfKm;
+if (age < 18) {
     price *= 0.8;
-} else {
-    if (age>=65) {
+} else if (age >= 65) {
         price *= 0.6;
     }
 }
 const finalPrice = price.toFixed(2);
 console.log(`Prezzo Finale: ${finalPrice}€`);
 
-if (isNaN(age) || isNaN(distance) || age <= 0 || distance <= 0) {
-    console.log("❌ Errore! Devi inserire numeri validi maggiori di 0.❌");}
